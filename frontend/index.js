f9e93f1ebf1f767f50611952d1d88dd7b52853c3
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-01-16 22:26:13 +0530

    oh yeah, something

--- frontend/index.js
+++ frontend/index.js
@@ -10,12 +10,35 @@ tile.innerHTML = "Hey";
 container.append(tile);
 winContainer.append(container);
 
+i = 0;
+cols = 2;
+rows = 1;
+
 window.addEventListener('keydown', (event) => {
   if (event.key == 'Enter' && event.altKey) {
-    const tile = document.createElement('div');
-    tile.classList.add('tile');
-    tile.innerHTML = "Hey";
+    let currContainer = document.getElementsByClassName('container')[i];
+    currContainer.style.gridTemplateColumns = `repeat(${i % 2 === 0 ? 1 : 2}, 1fr)`;
+    currContainer.style.gridTemplateRows = `repeat(${i % 2 !== 0 ? 1 : 2}, 1fr)`;
 
-    winContainer.append(tile);
+    const prevTile = currContainer.querySelector('.tile');
+    prevTile.remove();
+
+    const newTile = document.createElement('div');
+    newTile.classList.add('tile');
+    newTile.innerHTML = "Hey";
+
+    const newContainerFirst = document.createElement('div');
+    newContainerFirst.classList.add('container');
+    newContainerFirst.append(prevTile);
+
+    const newContainerSecond = document.createElement('div');
+    newContainerSecond.classList.add('container');
+    newContainerSecond.append(newTile);
+
+
+    currContainer.append(newContainerFirst, newContainerSecond);
+
+    i++;
+
   }
 })
